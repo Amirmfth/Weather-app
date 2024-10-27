7502be4668e253bbf3f67d778d11801bf664c9f9
Amirmfth <102855219+Amirmfth@users.noreply.github.com>
2024-10-27 12:01:26 +0330

    cleaned code and handled errors using modal

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const API_KEY = "1a32f78102ae1c400d50243389cd8a3b";
-const BASE_URL = "https://api.openweathermap.org/data/2.5/";
+import getWeatherData from "./utils/httpReg.js";
+import { removeModal, showModal } from "./utils/modal.js";
+
+const DAYS = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 const searchInp = document.querySelector("input");
 const searchBtn = document.querySelector("button");
 const weatherContainer = document.getElementById("weather");
-const locationIcon = document.getElementById("location")
-
-const gerCurrentWeatherByName = async (city) => {
-  const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`;
-  const response = await fetch(url);
-  const json = await response.json();
-  return json;
-};
-const gerCurrentWeatherBycoordinates = async (lat , lon) => {
-  const url = `${BASE_URL}/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  const response = await fetch(url);
-  const json = await response.json();
-  return json;
-};
-
-
+const forecastContainer = document.getElementById("forecast");
+const locationIcon = document.getElementById("location");
+const modalButton = document.getElementById("modal-button")
 
 const renderCurrentWeather = (data) => {
+    if(!data) return
   const weatherJSX = `
         <h1>${data.name} , ${data.sys.country}</h1>
         <div id="main">
-            <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png"  alt="weather icon"/>
+            <img src="https://openweathermap.org/img/w/${
+              data.weather[0].icon
+            }.png"  alt="weather icon"/>
             <span>${data.weather[0].main}</span>
             <p>${Math.round(data.main.temp)}°C</p>
         </div>
@@ -37,32 +37,61 @@ const renderCurrentWeather = (data) => {
   weatherContainer.innerHTML = weatherJSX;
 };
 
+const getWeekDay = (date) => {
+  return DAYS[new Date(date * 1000).getDay()];
+};
+
+const renderForecastWeather = (data) => {
+    if(!data) return
+  forecastContainer.innerHTML = "";
+  data = data.list.filter((obj) => obj.dt_txt.endsWith("12:00:00"));
+  data.forEach((i) => {
+    const forecastJSX = `
+            <div>
+                <img src="https://openweathermap.org/img/w/${
+                  i.weather[0].icon
+                }.png"  alt="weather icon"/>
+                <h3>${getWeekDay(i.dt)}</h3>
+                <p>${Math.round(i.main.temp)}°C</p>
+                <span>${i.weather[0].main}</span>
+            </div>
+        `;
+    forecastContainer.innerHTML += forecastJSX;
+  });
+};
+
 const searchHandler = async () => {
   const cityName = searchInp.value;
   if (!cityName) {
-    alert("Please enter city name");
+    showModal("Please enter city name");
+    return
   }
-  const currentData = await gerCurrentWeatherByName(cityName);
+  const currentData = await getWeatherData("current", cityName);
+  const forecastData = await getWeatherData("forecast", cityName);
   renderCurrentWeather(currentData);
+  renderForecastWeather(forecastData);
 };
 
 const positionCB = async (position) => {
-    const {latitude , longitude} = position.coords
-    const currentData = await gerCurrentWeatherBycoordinates(latitude , longitude)
-    renderCurrentWeather(currentData)
-}
+  const currentData = await getWeatherData("current", position.coords);
+  const forecastData = await getWeatherData("forecast", position.coords);
+  renderCurrentWeather(currentData);
+  renderForecastWeather(forecastData);
+};
 
 const errorCB = (error) => {
-    console.log(error.message)
-}
+  console.log(error.message);
+};
 
 const locationHandler = () => {
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(positionCB , errorCB)
-    }else {
-        alert("Your browser does not support geolocation")
-    }
-}
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(positionCB, errorCB);
+  } else {
+    showModal("Your browser does not support geolocation");
+    return
+  }
+};
 
 searchBtn.addEventListener("click", searchHandler);
-locationIcon.addEventListener("click" , locationHandler)
+locationIcon.addEventListener("click", locationHandler);
+modalButton.addEventListener("click" , removeModal)
---
+++ utils/httpReg.js
@@ -0,0 +1,39 @@
+import { showModal } from "./modal.js";
+
+const API_KEY = "1a32f78102ae1c400d50243389cd8a3b";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/";
+
+const getWeatherData = async (type, data) => {
+  let url = null;
+  switch (type) {
+    case "current":
+      if (typeof data === "string") {
+        url = `${BASE_URL}/weather?q=${data}&appid=${API_KEY}&units=metric`;
+      } else {
+        url = `${BASE_URL}/weather?lat=${data.latitude}&lon=${data.longitude}&appid=${API_KEY}&units=metric`;
+      }
+      break;
+    case "forecast":
+      if (typeof data === "string") {
+        url = `${BASE_URL}/forecast?q=${data}&appid=${API_KEY}&units=metric`;
+      } else {
+        url = `${BASE_URL}/forecast?lat=${data.latitude}&lon=${data.longitude}&appid=${API_KEY}&units=metric`;
+      }
+
+      break;
+  }
+
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    if (+json.cod === 200) {
+      return json;
+    } else {
+      showModal(json.message)
+    }
+  } catch (error) {
+    showModal("An error accured when fetching data")
+  }
+};
+
+export default getWeatherData;
